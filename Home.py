26d12770c50fc3646f40c9bae83ae2c80871aeed
Kitwana Akil <kakil@me.com>
2024-05-19 12:11:34 -0400

    Updated kit_photo image size.

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/kit_photo.jpg")
+    st.image("images/kit_photo.jpg", width=256)
 
 with col2:
     st.title("Kitwana Akil")
