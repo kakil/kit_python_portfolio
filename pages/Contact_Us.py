9ae275bf7bd2c5201f87c8b0cb1aeaa26921312e
Kitwana Akil <kakil@me.com>
2024-05-18 23:58:15 -0400

    Updated the Contact_Us.py to remove the email submission for the deployed app.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -14,8 +14,14 @@ From: {user_email}
 {raw_message}
 """
     # message = message + "\n" + user_email
-    button = st.form_submit_button("Submit")
+    button = st.form_submit_button("Submit", disabled=True)
 
     if button:
         send_email(message)
         st.info("Your email was sent successfully")
+
+st.markdown("*I have disable the submit button for the deployed application."
+            "  It requires opening an additional port on the server, and for"
+            " safety reasons I prefer to limit the open ports."
+            "  You can download the source code and run it to see the"
+            "functioning email form.*")
